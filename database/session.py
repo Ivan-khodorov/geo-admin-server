5d0ea92e5e85fd213f48ec8d8b96223351ea0a53
Ivan-khodorov <khodorov.ivan@gmail.com>
2025-04-09 16:23:27 +0700

    Update session.py

--- database/session.py
+++ database/session.py
@@ -1,8 +1,10 @@
 import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-# Асинхронная сессия
+engine = create_async_engine(DATABASE_URL, echo=True)
+
 async_session = async_sessionmaker(
     bind=engine,
     expire_on_commit=False
